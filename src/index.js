241931f151f98c2b333b126887c307918b0d6f87
andy_hardy <aohardy@gmail.com>
2019-07-11 18:04:28 -0600

    begin again after some footer work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     import main from './main';
     import portfolio from './portfolio';
+
+    // initialize the carousel
+    var options = {
+        numVisible: 3
+    };
+    portfolio.init(options);
     
-    $("#farts").text(portfolio.text);
+    // grab carousel information from the db and display it.
+
+    $.ajax({
+        method: "GET",
+        url: "/api/profile"
+    }).then(function(data) {
+        portfolio.addLinks(data);
+    });
 });
 
