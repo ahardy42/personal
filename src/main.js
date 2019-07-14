6995c324d918339a94971582583f8d345e1b883e
andy_hardy <aohardy@gmail.com>
2019-07-14 16:26:39 -0600

    woops. did some work on the master...

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-// initialize the carousel
-var options = {
-    numVisible: 3
-};
-portfolio.init(options);
-
+var portfolio = require("./portfolio");
 // grab carousel information from the db and display it.
 
 $.ajax({
     method: "GET",
     url: "/api/portfolio"
-}).then(function(data) {
-    portfolio.addLinks(data);
+}).then(function (data) {
+    console.log(data);
+    var linkArray = portfolio.addLinks(data);
+    linkArray.forEach(function(link) {
+        $(".carousel").append(link);
+    });
+    // initialize the carousel
+    var options = {
+        numVisible: 3,
+        dist: -50
+    };
+    var instances = portfolio.init(options);
+    // instances[0].itemHeight = 400;
+    console.log(instances);
 });
--- src/portfolio.js
+++ src/portfolio.js
@@ -8,17 +8,56 @@ var portfolio = {
         return instances;
     },
     addLinks: function(array) {
-        var linkArray = [];
+        console.log(array);
+        var cardArray = [];
         array.forEach(function(element) {
-            var link = document.createElement("a");
+            // card wrapper
+            var card = document.createElement("div");
+            card.classList.add("card", "carousel-item");
+            // card image wrapper
+            var cardImage = document.createElement("div");
+            cardImage.classList.add("card-image", "waves-effect", "waves-block", "waves-light");
             var img = document.createElement("img");
-            link.className = "carousel-item";
-            link.href = element.href;
-            img.src = element.imgUrl;
-            link.appendChild(img);
-            linkArray.push(link);
+            img.src = element.fileName;
+            img.classList.add("activator");
+            cardImage.appendChild(img);
+            // card content wrapper
+            var content = document.createElement("div");
+            content.classList.add("card-content");
+            var title1 = document.createElement("span");
+            title1.classList.add("card-title", "activator", "grey-text", "text-darken-4");
+            title1.textContent = element.title;
+            content.appendChild(title1);
+            console.log(content);
+            // reveal contents
+            var reveal = document.createElement("div");
+            reveal.classList.add("card-reveal");
+            var title2 = document.createElement("span");
+            title2.classList.add("card-title", "grey-text", "text-darken-4");
+            title2.textContent = element.title;
+            reveal.appendChild(title2);
+            var description = document.createElement("p");
+            description.setAttribute("style", "overflow: auto");
+            description.textContent = element.description;
+            reveal.appendChild(description);
+            // build the links out
+            var gitLink = document.createElement("a");
+            gitLink.innerHTML = "<i class=\"fab fa-github\"></i>";
+            gitLink.href = element.gitUrl;
+            gitLink.target = "_blank";
+            var projectLink = document.createElement("a");
+            projectLink.innerText = "Project Page";
+            projectLink.href = element.projectUrl;
+            projectLink.target = "_blank";
+            // attach links to card reveal
+            reveal.appendChild(gitLink);
+            reveal.appendChild(projectLink);
+            card.appendChild(cardImage);
+            card.appendChild(content);
+            card.appendChild(reveal);
+            cardArray.push(card);
         });
-        return linkArray;
+        return cardArray;
     }
 };
 
