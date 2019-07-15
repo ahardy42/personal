a8f5db19c5c1d2fb5af40a9c8c602939adf5144f
andy_hardy <aohardy@gmail.com>
2019-07-14 20:11:03 -0600

    removed signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,16 +1,8 @@
 $(document).ready(function () {
 
-    // checkbox check
-    $("#isSignup").on("click", function(e) {
-        var checked = e.target.checked;
-        var jqChk = $(this).attr("checked");
-        console.log(jqChk);
-    })
-
     // listener for submit
     $("form").on("submit", function (e) {
         e.preventDefault();
-        var isChecked = document.getElementById("isSignup").checked;
         var data = {
             username: $("#username").val().trim(),
             password: $("#password").val()
@@ -19,26 +11,14 @@ $(document).ready(function () {
             return;
         }
 
-        if (isChecked) {
-            $.ajax({
-                method: "POST",
-                url: "/api/signup",
-                data: data
-            }).then(function (response) {
-                window.location.assign("/admin");
-            }).catch(function (err) {
-                console.error(err);
-            });
-        } else {
-            $.ajax({
-                method: "POST",
-                url: "/api/login",
-                data: data
-            }).then(function (response) {
-                window.location.assign("/admin");
-            }).catch(function (err) {
-                console.error(err);
-            });
-        }
+        $.ajax({
+            method: "POST",
+            url: "/api/login",
+            data: data
+        }).then(function (response) {
+            window.location.assign("/admin");
+        }).catch(function (err) {
+            console.error(err);
+        });
     })
 });
