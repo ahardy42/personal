93d824537b98961aa32963cbf06dca2856136275
andy_hardy <aohardy@gmail.com>
2019-07-14 19:31:41 -0600

    ran production webpack build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-$(document).ready(function() {
     var M = require("materialize-css");
     // var $ = require("jquery");
     var portfolio = require("./portfolio");
     var main = require("./main");
-
-});
 
 
