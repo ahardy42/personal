9ef55cc6f1b96a48849a8301734a0477121bdb87
andy_hardy <aohardy@gmail.com>
2019-09-07 08:53:19 -0600

    updated conditional css addition

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 var portfolio = require("./portfolio");
 // grab carousel information from the db and display it.
 
+if (navigator.platform === "iPhone") {
+    var head = $("head");
+    console.log(head.children("link:last"));
+    head.children("link:last").after("<link rel='stylesheet' href='/style/iphone.css' type='text/css' />");
+}
+
 $.ajax({
     method: "GET",
     url: "/api/portfolio"
 }).then(function (data) {
-    console.log(data);
     var linkArray = portfolio.addLinks(data);
     linkArray.forEach(function(link) {
         $(".carousel").append(link);
